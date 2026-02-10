5409f74a2eb63b15d45e734c78162aab8543f720
unknown <chirag.garg.cse22@itbhu.ac.in>
2026-02-10 14:41:51 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,12 @@ int main() {
   std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
+  string command;
+
+  while(true){
   std::cout << "$ ";
   
-  string command;
   getline(cin, command);
   cout << command<<": command not found\n";
+  }
 }
