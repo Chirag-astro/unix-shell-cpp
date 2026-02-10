329866659b9266c7d96cdcdf458f05bf19ad24dd
unknown <chirag.garg.cse22@itbhu.ac.in>
2026-02-10 15:22:04 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <unordered_set>
 using namespace std;
 
 int main() {
@@ -9,6 +10,7 @@ int main() {
 
   // TODO: Uncomment the code below to pass the first stage
   string command;
+  unordered_set<string>builtin_commands = {"echo", "type", "exit"};  
 
   while(true){
     std::cout << "$ ";
@@ -18,6 +20,13 @@ int main() {
       break;
     }else if( command.length() >= 5 && command.substr(0,4) == "echo" && command[4] == ' '){
       cout << command.substr(5) <<"\n";
+    }else if( command.length() >= 5 && command.substr(0,4) == "type" && command[4] == ' '){
+       string arg = command.substr(5);
+       if(builtin_commands.find(arg)!= builtin_commands.end()){
+          cout << arg << " is a shell builtin\n";
+       }else{
+          cout << arg<<": not found\n";
+       }
     }else{
       cout << command<<": command not found\n";
     }
