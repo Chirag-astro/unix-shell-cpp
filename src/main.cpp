e176786d304c684ae094b7dc514d66cd1ef08947
unknown <chirag.garg.cse22@itbhu.ac.in>
2026-05-19 17:00:12 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -382,35 +382,6 @@ void find_all_executables()
 
 vector<string>hist;
 
-void write_history(string fname){
-  int fd = open(fname.c_str(), O_WRONLY, O_CREAT, O_APPEND, 0644);
-
-  for (int i = 0; i < hist.size(); i++)
-  {
-     string s = hist[i]+'\n';
-     write(fd, s.c_str(), s.size());
-  }
-  close(fd);
-  
-
-}
-
-void read_history(string fname){
-  int fd = open(fname.c_str(), O_RDONLY);
-  char c;
-  string s;
-  while(read(fd, &c, 1) > 0){
-    if(c=='\n'){
-      hist.push_back(s);
-      s.clear();
-    }else{
-      s.push_back(c);
-    }
-  }
-  if(!s.empty())hist.push_back(s);
-  close(fd);
-}
-
 int main()
 {
   // Flush after every std::cout / std:cerr
@@ -535,10 +506,24 @@ int main()
           }
         }else if(args[0] == "history"){
 
-          if(args.size()> 2 && args[1] == "-r"){
-              read_history(args[2]);
-          }else if(args.size() > 2 && args[1] == "-w"){
-              write_history(args[2]);
+          if(args[1] == "-r"){
+            string fname = args[2];
+            int fd = open(fname.c_str(), O_RDONLY);
+            char c;
+            string s;
+
+            while (read(fd,&c, 1) > 0)
+            {
+               if(c == '\n')
+               {
+                  hist.push_back(s);
+                  s.clear();
+               }else{ 
+                  s.push_back(c);
+               }
+            }
+            if(!s.empty())hist.push_back(s);
+            continue;
           }
 
           int limit = hist.size();
